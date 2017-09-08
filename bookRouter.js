e357026893bd03a091caf1a06966ee1f1b758c15
timetzhang <367271@qq.com>
2017-09-08 15:29:38 +0800

    backup and

--- bookRouter.js
+++ bookRouter.js
@@ -17,6 +17,9 @@ const formidable = require('formidable');
 const datetime = require('./datetime')
 const ObjectId = require('mongodb').ObjectId;
 
+const cheerio = require('cheerio'); //html parser
+const httpClient = require('https');
+
 router.use(logger('dev'));
 router.use(bodyParser.urlencoded({ extended: true }));
 
@@ -71,6 +74,46 @@ router.post('/newBook', async(req, res) => {
     res.send(result);
 });
 
+/**
+ * 从douban.com爬虫到书的内容
+ */
+router.get('/searchOnlineBook', async(req, res) => {
+    setHeadJson(res);
+    var book = {};
+
+    httpClient.get('https://book.douban.com/subject_search?search_text=' + req.query.search, result => {
+        var html = ''; //用来存储请求网页的整个html内容
+        var titles = [];
+        result.setEncoding('utf-8'); //防止中文乱码
+        //监听data事件，每次取一块数据
+        result.on('data', function(chunk) {
+            html += chunk;
+        });
+        //监听end事件，如果整个网页内容的html都获取完毕，就执行回调函数
+        result.on('end', function() {
+            var $ = cheerio.load(html); //采用cheerio模块解析html
+            book.name = $('h2 a:first-child').text().trim(); //书名
+            book.image = $('img:first-child').attr('src'); //书图片
+
+            var pub = $('.pub').text().trim().split('/');
+            if (pub.length > 4) { //有2个作者
+                book.author = pub[0].trim() + '/' + pub[1].trim();
+                book.publisher = pub[2].trim();
+                book.year = pub[3].trim();
+                book.price = pub[4].trim();
+            } else {
+                book.author = pub[0].trim();
+                book.publisher = pub[1].trim();
+                book.year = pub[2].trim();
+                book.price = pub[3].trim();
+            }
+
+            res.send(book);
+        })
+    });
+
+
+});
 // router.get('/getArticleDetails', async(req, res) => {
 //     setHeadJson(res)
 //         // get datas
